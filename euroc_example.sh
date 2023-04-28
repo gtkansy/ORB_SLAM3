2a18c6c6a625d5157a19eadf97e04761d3a9b8b3
leon <gtkansy@163.com>
2023-04-28 18:19:54 +0800

    fix compile issue and add example sh

---
+++ euroc_example.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pathDatasetEuroc='dataset/' #Example, it is necesary to change it by the dataset path
+#------------------------------------
+# Stereo-Inertial Examples
+echo "Launching MH01 with Stereo-Inertial sensor"
+./Examples/Stereo/stereo_euroc ./Vocabulary/ORBvoc.txt ./Examples/Stereo/EuRoC.yaml ~/"$pathDatasetEuroc"/MH_04_difficult ./Examples/Stereo/EuRoC_TimeStamps/MH04.txt dataset-MH04_stereo
